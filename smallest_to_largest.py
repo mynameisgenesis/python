4e77ac9810ac873485da08114b0f0637df3db45a
Genesis Murillo <genesis.mrll@gmail.com>
2020-02-03 20:55:00 -0700

    removed comments

--- smallest_to_largest.py
+++ smallest_to_largest.py
@@ -1,25 +1,3 @@
-# 4.18 LAB: Smallest and largest numbers in a list
-# Write a program that reads a list of integers into a list as long as the integers are greater than zero, 
-# then outputs the smallest and largest integers in the list.
-
-# Ex: If the input is:
-
-# 10
-# 5
-# 3
-# 21
-# 2
-# -6
-# the output is:
-
-# 2 21
-# You can assume that the list of integers will have at least 2 values.
-
-# LAB
-# ACTIVITY
-# 4.18.1: LAB: Smallest and largest numbers in a list
-# 0 / 10
-
 user_input = input()
 number_list = []
 while not(user_input == 'quit' or user_input == 'Quit' or user_input == 'q'):
