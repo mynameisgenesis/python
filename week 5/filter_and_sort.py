3c2ceead93dca93f12f1fc2e0af286c39244c07b
Genesis Murillo <genesis.mrll@gmail.com>
2020-02-17 21:19:47 -0700

    read the lab wrong | corrected the output

--- week 5/filter_and_sort.py
+++ week 5/filter_and_sort.py
@@ -1,7 +1,10 @@
 def get_nums():
     user_list = []
     user_nums = list(map(int, input().split())) 
-    user_list =  [abs(ele) for ele in user_nums]
+    #user_list =  [abs(ele) for ele in user_nums]
+    for num in user_nums:
+        if num >= 0:
+            user_list.append(num)
     return sorted(user_list)
 
 new_list = get_nums()
